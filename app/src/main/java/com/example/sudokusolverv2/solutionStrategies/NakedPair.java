d80642ea2d22c09e365504864db12d016aa7e88f
jannikbuehring <jannik.buehring1@web.de>
2021-04-08 11:44:26 +0200

    added naked pair for cols

--- app/src/main/java/com/example/sudokusolverv2/solutionStrategies/NakedPair.java
+++ app/src/main/java/com/example/sudokusolverv2/solutionStrategies/NakedPair.java
@@ -4,19 +4,20 @@ import com.example.sudokusolverv2.FieldCandidates;
 import com.example.sudokusolverv2.Solver;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 
 public class NakedPair {
 
     private Solver solver;
+
+    //TODO: implement a system to know if a naked pair has already been discovered before
 
     public boolean applyNakedPairToCandidates() {
         FieldCandidates[] nakedPair = getNakedPair();
         if(nakedPair == null) {
             return false;
         }
+        boolean removedSomething = false;
         if(nakedPair[0].row == nakedPair[1].row) {
-            boolean removedSomething = false;
             for(int c = 0; c < 9; c++) {
                 if(c == nakedPair[0].column || c == nakedPair[1].column) {
                     continue;
@@ -30,6 +31,23 @@ public class NakedPair {
                 return true;
             }
         }
+
+        if(nakedPair[0].column == nakedPair[1].column) {
+            for(int r = 0; r < 9; r++) {
+                if(r == nakedPair[0].row || r == nakedPair[1].row) {
+                    continue;
+                }
+                if(solver.calculatedCandidates.get(r * 9 + nakedPair[0].column)
+                        .removeAll(nakedPair[0].candidateSet)) {
+                    removedSomething = true;
+                }
+            }
+            if(removedSomething) {
+                return true;
+            }
+        }
+
+
         return false;
     }
 
@@ -37,6 +55,9 @@ public class NakedPair {
         FieldCandidates[] nakedPair;
         if (getNakedPairInRow() != null) {
             nakedPair = getNakedPairInRow();
+            return nakedPair;
+        } else if (getNakedPairInColumn() != null) {
+            nakedPair = getNakedPairInColumn();
             return nakedPair;
         }
 
@@ -48,6 +69,36 @@ public class NakedPair {
         for (int r = 0; r < 9; r++) {
             ArrayList<FieldCandidates> completeRow = new ArrayList<>();
             for (int c = 0; c < 9; c++) {
+                if (solver.board[r][c] == 0) {
+                    FieldCandidates candidateSet = new FieldCandidates(solver.calculatedCandidates.get(r * 9 + c));
+                    candidateSet.row = r;
+                    candidateSet.column = c;
+                    completeRow.add(candidateSet);
+                }
+            }
+            for (FieldCandidates candidates : completeRow) {
+                for (FieldCandidates candidates2 : completeRow) {
+                    if (candidates == candidates2) {
+                        continue;
+                    }
+                    if(candidates.candidateSet.size() == 2 && candidates2.candidateSet.size() == 2) {
+                        if(candidates.candidateSet.equals(candidates2.candidateSet)) {
+                            FieldCandidates[] nakedPair = new FieldCandidates[2];
+                            nakedPair[0] = candidates;
+                            nakedPair[1] = candidates2;
+                            return nakedPair;
+                        }
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    public FieldCandidates[] getNakedPairInColumn() {
+        for (int c = 0; c < 9; c++) {
+            ArrayList<FieldCandidates> completeRow = new ArrayList<>();
+            for (int r = 0; r < 9; r++) {
                 if (solver.board[r][c] == 0) {
                     FieldCandidates candidateSet = new FieldCandidates(solver.calculatedCandidates.get(r * 9 + c));
                     candidateSet.row = r;
--- app/src/test/java/com/example/sudokusolverv2/NakedPairTest.java
+++ app/src/test/java/com/example/sudokusolverv2/NakedPairTest.java
@@ -33,6 +33,33 @@ public class NakedPairTest extends TestCase {
         assertEquals(nakedPairSolution[1].column, 5);
     }
 
+    public void testGetNakedPairInColumn() {
+        Solver solver = new Solver();
+        NakedPair nakedPair = new NakedPair();
+        nakedPair.setSolver(solver);
+        solver.board = new int[][]{
+                {0, 0, 1, 4, 9, 0, 7, 0, 0},
+                {0, 0, 4, 0, 0, 6, 9, 3, 0},
+                {8, 0, 9, 0, 1, 7, 5, 4, 0},
+                {2, 8, 0, 0, 7, 0, 0, 5, 9},
+                {4, 1, 7, 9, 0, 0, 2, 6, 8},
+                {5, 9, 0, 0, 2, 0, 0, 0, 0},
+                {0, 4, 5, 0, 8, 0, 0, 0, 3},
+                {0, 0, 8, 7, 0, 0, 0, 0, 0},
+                {0, 0, 2, 0, 0, 9, 8, 0, 0}
+        };
+        solver.calculateInitialCandidates();
+        FieldCandidates[] nakedPairSolution = nakedPair.getNakedPairInColumn();
+
+        //Naked pair is in col 4
+        assertEquals(nakedPairSolution[0].column, 2);
+        assertEquals(nakedPairSolution[1].column, 2);
+
+        //And in rows 1 and 4
+        assertEquals(nakedPairSolution[0].row, 3);
+        assertEquals(nakedPairSolution[1].row, 5);
+    }
+
     public void testApplyNakedPairToCandidates() {
         Solver solver = new Solver();
         NakedPair nakedPair = new NakedPair();
