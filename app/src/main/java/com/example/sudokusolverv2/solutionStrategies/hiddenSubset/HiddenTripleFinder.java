f79731977b2172f9c9160cb40c1ed22fbbe0582f
jannikbuehring <jannik.buehring1@web.de>
2021-05-07 17:47:57 +0200

    fixed hidden triple for rows

--- app/src/main/java/com/example/sudokusolverv2/solutionStrategies/hiddenSubset/HiddenTripleFinder.java
+++ app/src/main/java/com/example/sudokusolverv2/solutionStrategies/hiddenSubset/HiddenTripleFinder.java
@@ -9,6 +9,7 @@ import org.apache.commons.lang3.SerializationUtils;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 
 public class HiddenTripleFinder implements Serializable {
@@ -161,9 +162,95 @@ public class HiddenTripleFinder implements Serializable {
         }
     }
 
-    // https://www.youtube.com/watch?v=VEmdzUaBpBQ
+    private Integer checkForOneRemainingCandidate(ArrayList<FieldCandidates> completeUnit) {
+
+        for (FieldCandidates candidates : completeUnit) {
+            if (candidates.candidateSet.size() == 1) {
+                Iterator<Integer> iterator = candidates.candidateSet.iterator();
+                return iterator.next();
+            }
+        }
+
+        return null;
+    }
+
+    // https://www.youtube.com/watch?v=mwoy-1B4qYw
     public HiddenTriple getHiddenTripleInRow() {
-        /*
+
+        for (int r = 0; r < 9; r++) {
+
+            // get a list of all current candidates for this row
+            ArrayList<FieldCandidates> completeRow = new ArrayList<>();
+            for (int c = 0; c < 9; c++) {
+                if (solver.board[r][c] == 0) {
+                    // we need to make a deep copy of the candidates because we want to edit it
+                    HashSet<Integer> candidates = SerializationUtils.clone(solver.calculatedCandidates.get(r * 9 + c));
+                    FieldCandidates candidateSet = new FieldCandidates(r, c, candidates);
+                    completeRow.add(candidateSet);
+                }
+            }
+
+            // if a number occurs more than three times, it cannot be part of a hidden triple
+            for (int i = 1; i < 10; i++) {
+                ArrayList<Integer> occurrences = new ArrayList<>();
+                for (FieldCandidates candidates : completeRow) {
+                    if (candidates.candidateSet.contains(i)) {
+                        occurrences.add(i);
+                    }
+                }
+                if (occurrences.size() > 3) {
+                    for (FieldCandidates candidates : completeRow) {
+                        candidates.candidateSet.remove(i);
+                    }
+                }
+            }
+
+            while (checkForOneRemainingCandidate(completeRow) != null) {
+                int remainingNumber = checkForOneRemainingCandidate(completeRow);
+                for (FieldCandidates candidates : completeRow) {
+                    candidates.candidateSet.remove(remainingNumber);
+                }
+            }
+
+            // if only two cells are left for consideration, it is not enough for a valid triple
+            if (completeRow.size() < 3) {
+                continue;
+            } else {
+                // check for a valid triple
+                for (FieldCandidates candidates : completeRow) {
+                    for (FieldCandidates candidates2 : completeRow) {
+                        for (FieldCandidates candidates3 : completeRow) {
+                            // making sure we are not comparing the same fields
+                            if (candidates == candidates2 || candidates == candidates3
+                                    || candidates2 == candidates3) {
+                                continue;
+                            }
+                            // if the size of the union set of all three sets is three, it should be
+                            // a valid triple
+                            HashSet<Integer> result = new HashSet<>(candidates.candidateSet);
+                            result.addAll(candidates2.candidateSet);
+                            result.addAll(candidates3.candidateSet);
+                            if (result.size() == 3 && candidates.candidateSet.size() >= 2
+                                    && candidates2.candidateSet.size() >= 2
+                                    && candidates3.candidateSet.size() >= 2) {
+                                HiddenTriple hiddenTriple = new HiddenTriple(candidates, candidates2,
+                                        candidates3, result);
+                                if (checkIfHiddenTripleCanRemoveCandidatesFromRow(hiddenTriple)) {
+                                    return hiddenTriple;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return null;
+    }
+
+    /*
+    public HiddenTriple getHiddenTripleInRow() {
+
         for (int r = 0; r < 9; r++) {
 
             // get a list of all current candidates for this row
@@ -247,10 +334,10 @@ public class HiddenTripleFinder implements Serializable {
                     }
                 }
             }
-        }*/
+        }
 
         return null;
-    }
+    }*/
 
     public HiddenTriple getHiddenTripleInColumn() {
         /*
